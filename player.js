94ab24380004550d6dff825848903fef42675ea6
Aaron Crespo <acrespo@nearinfinity.com>
2013-05-26 14:02:00 -0400

    error handling

--- player.js
+++ player.js
@@ -2,39 +2,54 @@ window.onresize = doLayout;
 var isLoading = false;
 
 onload = function() {
-  var webview = document.querySelector('webview');
+  var player = document.getElementById("player");
+  var loading = document.getElementById("loading");
+
+  var loadstart = function() {
+    resetExitedState();
+    loading.innerText = "Loading....";
+  }
+
+  var loadstop = function() {
+    loading.innerText = "";
+  }
+
+  player.addEventListener("loadstart", loadstart);
+  player.addEventListener("loadstop", loadstop);
+  player.addEventListener("exit", handleExit);
   doLayout();
 };
 
+window.onload = onload;
+
 function doLayout() {
-  var webview = document.querySelector('webview');
+  var player = document.getElementById("player");
   var windowWidth = document.documentElement.clientWidth;
   var windowHeight = document.documentElement.clientHeight;
   var webviewWidth = windowWidth;
   var webviewHeight = windowHeight;
 
-  webview.style.width = webviewWidth + 'px';
-  webview.style.height = webviewHeight + 'px';
+  player.style.width = webviewWidth + "px";
+  player.style.height = webviewHeight + "px";
 
-  var sadWebview = document.querySelector('#sad-webview');
-  sadWebview.style.width = webviewWidth + 'px';
-  sadWebview.style.height = webviewHeight * 2/3 + 'px';
-  sadWebview.style.paddingTop = webviewHeight/3 + 'px';
+  var sadWebview = document.querySelector("#sad-webview");
+  sadWebview.style.width = webviewWidth + "px";
+  sadWebview.style.height = webviewHeight * 2/3 + "px";
+  sadWebview.style.paddingTop = webviewHeight/3 + "px";
 }
 
 function handleExit(event) {
-  console.log(event.type);
-  document.body.classList.add('exited');
-  if (event.type == 'abnormal') {
-    document.body.classList.add('crashed');
-  } else if (event.type == 'killed') {
-    document.body.classList.add('killed');
+  document.body.classList.add("exited");
+  if (event.type == "abnormal") {
+    document.body.classList.add("crashed");
+  } else if (event.type == "killed") {
+    document.body.classList.add("killed");
   }
 }
 
 function resetExitedState() {
-  document.body.classList.remove('exited');
-  document.body.classList.remove('crashed');
-  document.body.classList.remove('killed');
+  document.body.classList.remove("exited");
+  document.body.classList.remove("crashed");
+  document.body.classList.remove("killed");
 }
 
